1c34b46800e92029f7ccd96a426748edf750d262
Vu Luong <vuluongj20@gmail.com>
2019-01-23 23:09:28 +0530

    Fixed JWT issuer discrepancy

--- back/mid/auth.js
+++ back/mid/auth.js
@@ -11,22 +11,22 @@ module.exports = async function(req, res, next) {
       var publicKey = await readFile('./data/login/public.key', 'utf8');
       var verified = await jwt.verify(cookie, publicKey, {
         algorithm: ['RS256'],
-        issuer: 'Lore'
+        issuer: 'Unknown'
       });
       if (verified) {
         res.locals.auth = {type: verified.type, account: verified.account};
         next();
       } else {
         res.clearCookie('jwt');
-        res.redirect('/login');
+        res.redirect('/login/');
       }
     } else {
-      res.redirect('/login');
+      res.redirect('/login/');
     }
   } catch (err) {
     if (err.name === 'TokenExpiredError') {
       res.clearCookie('jwt');
-      res.redirect('/login');
+      res.redirect('/login/');
     }
     next(err);
   }
